9f83e8f2bb273ff7476c25c0856d728f66526643
Pierre Riteau <priteau@gmail.com>
2010-04-02 13:29:48 +0200

    Better indentation of comment

--- lib/gosen.rb
+++ lib/gosen.rb
@@ -4,17 +4,22 @@ require 'gosen/deployment'
 require 'gosen/error'
 
 module Gosen
-  # Extract the site portion of the node name, and returns a hash indexing the nodes with their site
-  # Example: input is:
-  # [ 'paramount-1.rennes.grid5000.fr',
-  #   'paramount-2.rennes.grid5000.fr',
-  #   'grelon-1.nancy.grid5000.fr',
-  #   'grelon-2.nancy.grid5000.fr' ]
-  # Result is:
-  # {
-  #   'rennes' => [ 'paramount-1.rennes.grid5000.fr', 'paramount-2.rennes.grid5000.fr' ],
-  #   'nancy' => [ 'grelon-1.nancy.grid5000.fr', 'grelon-2.nancy.grid5000.fr' ]
-  # }
+  # Extracts the site part of a Grid'5000 node hostname, and returns a hash indexing the nodes by their site
+  # Example:
+  #
+  #   Input is:
+  #   [
+  #     'paramount-1.rennes.grid5000.fr',
+  #     'paramount-2.rennes.grid5000.fr',
+  #     'grelon-1.nancy.grid5000.fr',
+  #     'grelon-2.nancy.grid5000.fr'
+  #   ]
+  #
+  #   Result is:
+  #   {
+  #     'rennes' => [ 'paramount-1.rennes.grid5000.fr', 'paramount-2.rennes.grid5000.fr' ],
+  #     'nancy' => [ 'grelon-1.nancy.grid5000.fr', 'grelon-2.nancy.grid5000.fr' ]
+  #   }
   def self.index_nodes_by_site(nodes)
     result = Hash.new { |hash, key| hash[key] = Array.new }
     nodes.each do |node|
